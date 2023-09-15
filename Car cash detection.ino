62c1af894d3c1fcbb89109241c259a2afff6a182
Surin-B <103912269+Surin-B@users.noreply.github.com>
2023-09-15 09:35:55 +0700

    Update Car cash detection.ino

--- Car cash detection.ino
+++ Car cash detection.ino
@@ -14,25 +14,23 @@ Serial.println();
 Serial.println(F("DFRobot DFPlayer Mini Demo"));
 Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
  
-if (!myDFPlayer.begin(mySoftwareSerial)) { //Use softwareSerial to communicate with mp3.
-Serial.println(F("Unable to begin:"));
-Serial.println(F("1.Please recheck the connection!"));
-Serial.println(F("2.Please insert the SD card!"));
-while (true) {
-delay(0); // Code to compatible with ESP8266 watch dog.
-}
-}
+if (!myDFPlayer.begin(mySoftwareSerial)) //Use softwareSerial to communicate with mp3.
+  { 
+  Serial.println(F("Unable to begin:"));
+  Serial.println(F("1.Please recheck the connection!"));
+  Serial.println(F("2.Please insert the SD card!"));
+  while (true) 
+    {
+    delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
 Serial.println(F("DFPlayer Mini online."));
 myDFPlayer.volume(20); //ตั้งระดับความดังของเสียง 0-30
-
 mySerial.begin(9600);
-
 Serial.println("Initializing..."); 
 delay(1000);
-
 mySerial.println("AT"); //Once the handshake test is successful, i t will back to OK
 updateSerial();
-  
 mySerial.println("ATD+ +66885939942;"); //  change ZZ with country code and xxxxxxxxxxx with phone number to dial
 updateSerial();
 
@@ -40,21 +38,7 @@ updateSerial();
 
 void loop()
 {
-while (Serial.available()) {
-char inChar = (char)Serial.read();
-if (inChar == '1') {
-myDFPlayer.play(11);
-}
-if (inChar == '2') {
-myDFPlayer.play(12);
-}
-if (inChar == '3') {
-myDFPlayer.play(13);
-}
-else if (inChar == '0') {
-myDFPlayer.stop();
-}
-}
+// call function
 }
 
 void updateSerial()
@@ -68,4 +52,10 @@ void updateSerial()
   {
     Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
   }
+}
+
+void voice()
+{
+  myDFPlayer.play(13);
+  myDFPlayer.play(12);
 }
